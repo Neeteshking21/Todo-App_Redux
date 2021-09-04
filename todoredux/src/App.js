0bf6d993ccc7c611ea3705b1ba1367465f8994d4
NeeteshKing21 <neeteshking21@gmail.com>
2021-09-04 23:49:23 +0530

    Adding Redux and Front End

--- todoredux/src/App.js
+++ todoredux/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import './App.css'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='container'>
+        <div className='Todo_card'>
+          <h1>ToDo App</h1>
+          <form>
+            <input type='text'></input>
+            <button><ion-icon name="add-outline"></ion-icon></button>
+          </form>
+
+        {/* Todos List */}
+
+          <div className='todos_list'>
+                <input disabled></input>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
